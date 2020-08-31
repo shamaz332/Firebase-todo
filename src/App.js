1acb946d0310d2917c98574be40f599d5f002e1a
shamaz332 <shamazsaeed332@gmail.com>
2020-08-31 14:26:36 +0500

    adding static todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState(["let the dog bark"]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    setTodo([...todo, input]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React + Firebase todo App 💯😁 </h1>
+      <br />
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Save Task</button>
+      <ul>
+        {todo.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
