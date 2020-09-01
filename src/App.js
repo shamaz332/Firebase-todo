cc2d2ea79a207638b9352d770bef6190f2407fbc
shamaz332 <shamazsaeed332@gmail.com>
2020-09-01 14:08:13 +0500

    two way sync complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Button from "@material-ui/core/Button";
 import { Input, InputLabel, FormControl } from "@material-ui/core";
 import { Todo } from "./components/Todo";
 import database from "./firebase";
+import firebase from "firebase";
 function App() {
   const [todo, setTodo] = useState([]);
   const [input, setInput] = useState("");
@@ -11,16 +12,27 @@ function App() {
   // fetching data from firebase
 
   useEffect(() => {
-    database.collection("todo").onSnapshot((snapshot) => {
-      setTodo(snapshot.docs.map((doc) => doc.data().todo));
-    });
+    database
+      .collection("todos")
+      .orderBy("timeStamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodo(snapshot.docs.map((doc) => doc.data().todo));
+      });
   }, [input]);
 
   //Adding todo items
 
   const addTodo = (e) => {
     e.preventDefault();
-    setTodo([...todo, input]);
+
+    //below lines are pusing code to database
+    database.collection("todos").add({
+      todo: input,
+      timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    // below line is adding items locally(statically)
+    // setTodo([...todo, input]);
     setInput(""); //clear the input after hit submit
   };
 
@@ -48,8 +60,8 @@ function App() {
       </Button>
 
       <ul>
-        {todo.map((todo) => (
-          <Todo todo={todo} />
+        {todo.map((todo, index) => (
+          <Todo id={index} todo={todo} />
         ))}
       </ul>
     </div>
